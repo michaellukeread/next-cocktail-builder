a3605825cdac90dddf710a68031bc13215a06829
michaellukeread <mread100@gmail.com>
2022-09-22 17:58:36 +1000

    add serverSideProps

--- src/pages/cocktails/[id].tsx
+++ src/pages/cocktails/[id].tsx
@@ -1,9 +1,31 @@
-import { useCocktailById } from "features/cocktails/hooks/useCocktailById"
+import { NextPage, GetServerSideProps } from "next"
+import { dehydrate } from "@tanstack/react-query"
+
+import { useCocktailById } from "features/cocktails/hooks"
+import { queryClient } from "lib/queryClient"
+import { COCKTAIL_BY_ID_KEY } from "features/cocktails/config"
+
 import Image from "next/image"
 
-const Cocktail = ({ query }) => {
-  console.debug("query", query)
-  const { isLoading, isFetching, error, data } = useCocktailById(query.id)
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  await queryClient.prefetchQuery([COCKTAIL_BY_ID_KEY, query.id], () =>
+    useCocktailById(query.id as string)
+  )
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      id: query.id,
+    },
+  }
+}
+
+type Props = {
+  id: string
+}
+
+const Cocktail: NextPage<Props> = ({ id }) => {
+  const { isLoading, isFetching, error, data } = useCocktailById(id)
 
   if (isLoading || isFetching) return <div className="screen">Loading...</div>
   if (error) return <div>error...</div>
@@ -27,7 +49,5 @@ const Cocktail = ({ query }) => {
     </section>
   )
 }
-
-Cocktail.getInitialProps = async ({ query }) => ({ query })
 
 export default Cocktail
